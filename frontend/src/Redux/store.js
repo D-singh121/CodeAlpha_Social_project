3beaf3281bcaf4ddd1984f70bdaf8a5a93cfe89a
Devesh_singh <choudharydevesh121@gmail.com>
2024-07-20 00:09:00 +0530

    done with basic functionality

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,12 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
 import tweetSlice from "./tweetSlice";
 
-const store = configureStore({
-	reducer: {
-		user: userSlice,
-		tweet: tweetSlice
-	}
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+}
+const rootReducer = combineReducers({
+	user: userSlice,
+	tweet: tweetSlice,
+	whitelist: ["user", "tweet"]  // add which you want to allows
 })
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({ serializableCheck: false }),   // Disable serializability check 
+})
+const persistor = persistStore(store)
+
+export { store, persistor };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import { Provider } from "react-redux"
-import store from './Redux/store.js'
+import { store, persistor } from './Redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
+
 
